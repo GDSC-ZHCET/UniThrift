0b88c58c5b64fb8226c6a9232edae0d743d4d6b4
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2025-01-14 00:03:01 +0530

    :zap: Add [Navbar] add to cart logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import ProductsList from './components/ProductsList';
 
@@ -6,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className='w-[100%] min-h-screen text-black flex flex-col'>
+      <div className='w-[100%] min-h-screen pt-[10vh] text-black flex flex-col'>
         <h1 className="font-bold text-3xl sm:px-6 lg:px-8 py-10">Explore Products</h1>
         <ProductsList />
       </div>
